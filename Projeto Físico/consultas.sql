fd7aa74a3a22a4f53005a33c1777509f558b6d59
ribeirowski <enioribeiro9@hotmail.com>
2023-09-16 12:56:40 -0300

    update: reorganização dos procedures e functions

---
+++ Projeto Físico/consultas.sql
@@ -0,0 +1,34 @@
+-------------------------
+--------CONSULTAS--------
+-------------------------
+
+-- CONSULTA 1 ():
+
+
+
+-- CONSULTA 2 ():
+
+
+
+-- CONSULTA 3 ():
+
+
+
+-- CONSULTA 4 ():
+
+
+
+-- CONSULTA 5 ():
+
+
+
+-- CONSULTA 6 ():
+
+
+
+-- CONSULTA 7 ():
+
+
+
+-- CONSULTA 8 ():
+
---
+++ Projeto Físico/functions/verificar_salario.sql
@@ -0,0 +1,1 @@
+-- VERIFICAR O SALÁRIO
