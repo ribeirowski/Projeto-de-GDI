f21658df8f256fc001f3d3630d1dd295efeec6e3
Matheus Augusto <62677004+MatheusMnt@users.noreply.github.com>
2023-09-18 08:55:08 -0300

    Add files via upload
    ajustes nas funções

--- Projeto Físico/functions/verificar_idade.sql
+++ Projeto Físico/functions/verificar_idade.sql
@@ -1,14 +1,28 @@
---TODO : FASZER FUNCIONAR 
-CREATE OR REPLACE FUNCTION VERIFICAR_IDADE_FIM_DO_ANO (CPF_P VARCHAR2) RETURN INT IS
-    IDADE INT;
+CREATE OR REPLACE FUNCTION CALCULAR_IDADE(CPF_P VARCHAR2) RETURN NUMBER IS
+    IDADE NUMBER;
     DATA_NASCIMENTO DATE;
 BEGIN
-    SELECT TO_DATE(NASCIMENTO, 'dd/mm/yyyy') INTO DATA_NASCIMENTO
+    -- Converte a data de nascimento de VARCHAR2 para DATE
+    SELECT TO_DATE(NASCIMENTO, 'dd-Mon-yy') INTO DATA_NASCIMENTO
     FROM PIRATA
     WHERE CPF = CPF_P;
-	
-    -- Calcula a idade com base na diferença em meses entre a data de nascimento e a data atual
+
+    -- Calcula a idade com base na data de nascimento
     IDADE := TRUNC(MONTHS_BETWEEN(SYSDATE, DATA_NASCIMENTO) / 12);
+
+    -- Torna a idade positiva se for negativa
+    IF IDADE < 0 THEN
+        IDADE := IDADE * (-1);
+    END IF;
 
     RETURN IDADE;
 END;
+/
+
+-- --eXECUNTANDO
+-- DECLARE
+--     idade NUMBER;
+-- BEGIN
+--     idade := CALCULAR_IDADE('006');
+--     DBMS_OUTPUT.PUT_LINE('Idade do pirata: ' || idade || ' anos');
+-- END;
